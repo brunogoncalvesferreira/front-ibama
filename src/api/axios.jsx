f58d62c21922dec73279c2fd813b4e9d3a814067
brunogoncalvesferreira <brunogoncalvesferreira@outllok.com>
2024-05-31 14:31:52 -0300

    url em produção

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080',
-  // baseURL: 'https://api-ibama-kxuh4cg6oa-uw.a.run.app',
+  // baseURL: 'http://localhost:8080',
+  baseURL: 'https://api-ibama-kxuh4cg6oa-uw.a.run.app',
   withCredentials: true,
 })
